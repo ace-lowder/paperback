9677a4ab907c016791252f3bb11084a8e8d6ada5
Ace <ace.lowder@gmail.com>
2024-09-16 21:21:40 -0700

    Highlight membership button

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -3,15 +3,19 @@ const Topbar = () => {
 
   return (
     <div className="bg-secondary">
-      <div className="container">
-        {menuButtons.map((item, index) => (
-          <button
-            key={index}
-            className="py-4 px-8 text-xs text-white font-bold"
-          >
-            {item.toUpperCase()}
-          </button>
-        ))}
+      <div className="container flex justify-between">
+        <div>
+          {menuButtons.map((item, index) => (
+            <button
+              key={index}
+              className={`py-4 px-8 text-xs text-white font-bold tracking-wider ${
+                index === menuButtons.length - 1 ? "bg-primary" : ""
+              }`}
+            >
+              {item.toUpperCase()}
+            </button>
+          ))}
+        </div>
       </div>
     </div>
   );
