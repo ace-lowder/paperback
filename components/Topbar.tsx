ba04501cbe4ed82014c18736b45dc538e244a9a8
Ace <ace.lowder@gmail.com>
2024-09-16 21:32:15 -0700

    Add user buttons

--- components/Topbar.tsx
+++ components/Topbar.tsx
@@ -1,20 +1,30 @@
+import { FaRegUserCircle, FaRegHeart } from "react-icons/fa";
+
 const Topbar = () => {
   const menuButtons = ["Bestsellers", "Deals", "Gift Cards", "Membership"];
 
   return (
-    <div className="bg-secondary">
+    <div className="bg-secondary text-xs text-white font-bold tracking-wider">
       <div className="container flex justify-between">
         <div>
           {menuButtons.map((item, index) => (
             <button
               key={index}
-              className={`py-4 px-8 text-xs text-white font-bold tracking-wider ${
+              className={`py-4 px-8 ${
                 index === menuButtons.length - 1 ? "bg-primary" : ""
               }`}
             >
               {item.toUpperCase()}
             </button>
           ))}
+        </div>
+        <div className="flex">
+          <button className="flex px-6 gap-2 items-center">
+            <FaRegUserCircle size={16} /> SIGN IN
+          </button>
+          <button className="flex px-6 gap-2 items-center">
+            <FaRegHeart size={16} className="mb-0.5" /> WISHLIST
+          </button>
         </div>
       </div>
     </div>
