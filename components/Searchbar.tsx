6d609aec794167f797473e61b57494eeeced41a9
Ace <ace.lowder@gmail.com>
2024-09-27 11:52:17 -0700

    Add search button

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,5 +1,12 @@
 const Searchbar = () => {
-  return <div className="w-full h-9 outline outline-secondary">Searchbar</div>;
+  return (
+    <div className="w-full h-10 flex gap-6">
+      <div className="border border-[3px] border-secondary flex-grow">
+        Searchbar
+      </div>
+      <button className="bg-primary text-white px-12">Search</button>
+    </div>
+  );
 };
 
 export default Searchbar;
